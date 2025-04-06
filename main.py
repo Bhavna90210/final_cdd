4500127e63444957d5b74b7ddb744606c57e106f
Bhavna90210 <bhavnaprasad16@gmail.com>
2025-04-06 08:59:43 +0530

    Use compressed model with joblib

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app.add_middleware(
 )
 
 # Load your trained model
-model = joblib.load("qsar_model.pkl",mmap_mode='r')
+model = joblib.load('beta_secretase_model.pkl')
 
 @app.post("/predict")
 async def predict(data: dict):
